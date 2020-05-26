099803b7824209bca074f292b43033bd93783554
Андрей Ельцов <andreq64AhA@gmail.com>
2020-05-26 21:41:55 +0300

    Юниттесты SetMarks

--- email/18-ISbo-2b/unit_tests/unittest_SetMarks.py
+++ email/18-ISbo-2b/unit_tests/unittest_SetMarks.py
@@ -6,7 +6,39 @@ import unittest
 
 
 class test_SetMarks(unittest.TestCase):
+    """Перед запуском теста нужно стереть оценку за 10 лабораторную у Калининой Екатерины
+    в журнале https://docs.google.com/spreadsheets/d/1MPkVTspH5MCtCvUXNTduhgQl90N3LIjzLjqfjYTDPdc/edit#gid=867705603"""
 
+    def test_S_7_t3(self):
+        """В журнал выставляется ноль"""
+
+        # Создание ожидаемого результата
+        Student = User()
+        Student.NameOfStudent = 'Калинина Екатерина Алексеевна'
+        Student.GroupOfStudent = '18-ИСбо-2б'
+        Student.IsRegistered = True
+        letters = []
+        let = LetterResult()
+        let.Student = Student
+        let.IsOK = False
+        let.CodeStatus = '30'
+        let.NumberOfLab = 10
+        letters.append(let)
+
+        SetMarks(letters)
+
+        self.assertEqual(letters[0].CodeStatus, '30')
+
+    def test_S_7_t4(self):
+        # Создание ожидаемого результата
+        letters = []
+        let = LetterResult()
+        let.CodeStatus = '05'
+        letters.append(let)
+
+        SetMarks(letters)
+
+        self.assertEqual(letters[0].CodeStatus, '05')
 
     def test_S_7_t1(self):
         """В журнал выставляется единица"""
@@ -29,8 +61,8 @@ class test_SetMarks(unittest.TestCase):
         # Метод ничего не возвращает, но отрабатывает верно
         self.assertEqual(letters[0].CodeStatus, '30')
 
-
     def test_S_7_t2(self):
+        """Оценка в журнале не обновляется, т.к. работа была сдана ранее"""
 
         # Создание ожидаемого результата
         Student = User()
@@ -48,39 +80,6 @@ class test_SetMarks(unittest.TestCase):
         SetMarks(letters)
 
         self.assertEqual(letters[0].CodeStatus, '10')
-
-
-    def test_S_7_t3(self):
-
-        # Создание ожидаемого результата
-        Student = User()
-        Student.NameOfStudent = 'Калинина Екатерина Алексеевна'
-        Student.GroupOfStudent = '18-ИСбо-2б'
-        Student.IsRegistered = True
-        letters = []
-        let = LetterResult()
-        let.Student = Student
-        let.IsOK = False
-        let.CodeStatus = '30'
-        let.NumberOfLab = 10
-        letters.append(let)
-
-        SetMarks(letters)
-
-        self.assertEqual(letters[0].CodeStatus, '30')
-
-
-    def test_S_7_t4(self):
-
-        # Создание ожидаемого результата
-        letters = []
-        let = LetterResult()
-        let.CodeStatus = '05'
-        letters.append(let)
-
-        SetMarks(letters)
-
-        self.assertEqual(letters[0].CodeStatus, '05')
 
 
 if __name__ == '__main__':
