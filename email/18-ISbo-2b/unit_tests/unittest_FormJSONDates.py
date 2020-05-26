8dc3488f3c0d25cf3946035937472c6291b9890d
SkinnerX7 <gusevart200071@gmail.com>
2020-05-26 19:00:11 +0300

    Написаны юнит тесты на функцию FormJSONDates

--- email/18-ISbo-2b/unit_tests/unittest_FormJSONDates.py
+++ email/18-ISbo-2b/unit_tests/unittest_FormJSONDates.py
@@ -1,152 +1,92 @@
-import json
 import unittest
-import global_User as User
-import global_Letter as Letter
-import FormJSONDates
-
+import global_User
+import global_Letter
+import main_2_base_WorkWithLetters
+import json
 
 class MyTestCase(unittest.TestCase):
-    """Тестирование метода FormJSONDates"""
+    def test_S_5_t1(self):
+        """Лабораторная работа с номером из списка [2, 3, 11]"""
 
-    def test_function(self):
-        """Тест на общий функционал"""
-        student1 = User.User("Максим Расторгуев", "18-ИСбо-2", None, None)
-        student2 = User.User("Валерий Бублин", "18-ИСбо-2", None, None)
-        student3 = User.User("Артём Гусев", "18-ИСбо-2", None, None)
+        # Создание студента
+        student3 = global_User.User("Артём Гусев", "18-ИСбо-2", None, None)
 
+        # Список писем
         letters = []
-        letter1 = Letter.Letter(student1, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 1, 1)
-        letter1.Body = "Maxim_Rastorguev"
-        letter1.CodeStatus = "20"
-        letter2 = Letter.Letter(student2, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 2, 1)
-        letter2.Body = "Valeriy_Bublin"
-        letter2.CodeStatus = "20"
-        letter3 = Letter.Letter(student3, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 3, 1)
-        letter3.Body = "Artyom_Gusev"
-        letter3.CodeStatus = "20"
 
-        letters.append(letter1)
-        letters.append(letter2)
-        letters.append(letter3)
+        # Создание письма в соответствии со сценарием
+        letter = global_Letter.Letter(student3, "ЛР01", None, 1, 3)
+        letter.Body = "Artyom_Gusev"
+        letter.CodeStatus = "20"
 
-        jsonDates_received = FormJSONDates.FormJSONDates(letters)
+        letters.append(letter)
 
-        json1_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 1,
-                "link" : None,
-                "code" : "Maxim_Rastorguev"
-                }
-        json2_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 2,
-                "link" : None,
-                "code" : "Valeriy_Bublin"
-                }
+        # Формирование JSON
+        jsonDates_received = main_2_base_WorkWithLetters.FormJSONDates(letters)
+
+        # Формирование ожидаемого JSON
         json3_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 3,
-                "link" : None,
-                "code" : "Artyom_Gusev"
-                }
+            "messageType": 1,
+            "lab": 3,
+            "variant": 1,
+            "link": "Artyom_Gusev",
+            "code": None
+        }
 
-        jsonDates_expected = [json.dumps(json1_expected), json.dumps(json2_expected), json.dumps(json3_expected)]
+        jsonDates_expected = [json.dumps(json3_expected)]
+
+        self.assertEqual(jsonDates_received, jsonDates_expected)
+    def test_S_5_t2(self):
+        """Лабораторная работа с номером из списка [4, 5, 6, 7, 8, 9, 10, 12]"""
+
+        # Создание студента
+        student3 = global_User.User("Артём Гусев", "18-ИСбо-2", None, None)
+
+        # Список писем
+        letters = []
+
+        # Создание письма в соответствии со сценарием
+        letter = global_Letter.Letter(student3, "ЛР01", None, 1, 7)
+        letter.Body = "Artyom_Gusev"
+        letter.CodeStatus = "20"
+
+        letters.append(letter)
+
+        # Формирование JSON
+        jsonDates_received = main_2_base_WorkWithLetters.FormJSONDates(letters)
+
+        # Формирование ожидаемого JSON
+        json3_expected = {
+            "messageType": 1,
+            "lab": 7,
+            "variant": 1,
+            "link": None,
+            "code": "Artyom_Gusev"
+        }
+
+        jsonDates_expected = [json.dumps(json3_expected)]
 
         self.assertEqual(jsonDates_received, jsonDates_expected)
 
-    def test_CodeStatus(self):
-        """Тест на фильтрацию по CodeStatus"""
-        student1 = User.User("Максим Расторгуев", "18-ИСбо-2", None, None)
-        student2 = User.User("Валерий Бублин", "18-ИСбо-2", None, None)
-        student3 = User.User("Артём Гусев", "18-ИСбо-2", None, None)
+    def test_S_5_t3(self):
+        """Код статус не равен 20"""
 
+        # Создание студента
+        student3 = global_User.User("Артём Гусев", "18-ИСбо-2", None, None)
+
+        # Список писем
         letters = []
-        letter1 = Letter.Letter(student1, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 16, 1)
-        letter1.Body = "Maxim_Rastorguev"
-        letter1.CodeStatus = "03"
-        letter2 = Letter.Letter(student2, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 2, 1)
-        letter2.Body = "Valeriy_Bublin"
-        letter2.CodeStatus = "20"
-        letter3 = Letter.Letter(student3, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 3, 1)
-        letter3.Body = "Artyom_Gusev"
-        letter3.CodeStatus = "20"
 
-        letters.append(letter1)
-        letters.append(letter2)
-        letters.append(letter3)
+        # Создание письма в соответствии со сценарием
+        letter = global_Letter.Letter(student3, "ЛР01", None, 1, 3)
+        letter.Body = "Artyom_Gusev"
+        letter.CodeStatus = "02"
 
-        jsonDates_received = FormJSONDates.FormJSONDates(letters)
+        letters.append(letter)
 
-        json2_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 2,
-                "link" : None,
-                "code" : "Valeriy_Bublin"
-                }
-        json3_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 3,
-                "link" : None,
-                "code" : "Artyom_Gusev"
-                }
+        jsonDates_received = main_2_base_WorkWithLetters.FormJSONDates(letters)
 
-        jsonDates_expected = [json.dumps(json2_expected), json.dumps(json3_expected)]
-
-        self.assertEqual(jsonDates_received, jsonDates_expected)
-
-    def test_function(self):
-        """Тест на общий функционал"""
-        student1 = User.User("Максим Расторгуев", "18-ИСбо-2", None, None)
-        student2 = User.User("Валерий Бублин", "18-ИСбо-2", None, None)
-        student3 = User.User("Артём Гусев", "18-ИСбо-2", None, None)
-
-        letters = []
-        letter1 = Letter.Letter(student1, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 1, 1)
-        letter1.Body = "Maxim_Rastorguev"
-        letter1.CodeStatus = "20"
-        letter2 = Letter.Letter(student2, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 2, 1)
-        letter2.Body = "Valeriy_Bublin"
-        letter2.CodeStatus = "20"
-        letter3 = Letter.Letter(student3, "ЛР01", "https://github.com/Progoger/TasksForStudents/tree/master/18-ИСбо-2/ЛР01_test_py", 3, 1)
-        letter3.Body = "Artyom_Gusev"
-        letter3.CodeStatus = "20"
-
-        letters.append(letter1)
-        letters.append(letter2)
-        letters.append(letter3)
-
-        jsonDates_received = FormJSONDates.FormJSONDates(letters)
-
-        json1_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 1,
-                "link" : None,
-                "code" : "Maxim_Rastorguev"
-                }
-        json2_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 2,
-                "link" : None,
-                "code" : "Valeriy_Bublin"
-                }
-        json3_expected = {
-                "messageType" : 1,
-                "lab" : 1,
-                "variant" : 3,
-                "link" : None,
-                "code" : "Artyom_Gusev"
-                }
-
-        jsonDates_expected = [json.dumps(json1_expected), json.dumps(json2_expected), json.dumps(json3_expected)]
-
-        self.assertEqual(jsonDates_received, jsonDates_expected)
+        self.assertEqual(jsonDates_received, [])
 
 if __name__ == '__main__':
     unittest.main()
