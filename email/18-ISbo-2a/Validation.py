b0c01fb3fb219d9c373d56740170d2b7c72c8a65
Никита Обидин <majishpro@gmail.com>
2020-05-31 16:55:23 +0300

    Изменил валидацию

--- email/18-ISbo-2a/Validation.py
+++ email/18-ISbo-2a/Validation.py
@@ -4,75 +4,37 @@ from pattern import *
 
 
 @log_method.log_method_info
-def validation(head_of_msg,body_of_msg):
-    Errors_list = [] # Список ошибок
-    for x in SUBJECT_LIST: # Проверка на название предмета
-        a = head_of_msg.lower().find(x)
-        if a != -1:
-            break
-    if a == -1:
-        log_method.logger.warning('validation: The name of \
-                                   item is incorrect.')
-        Errors_list.append('неверно указано название предмета')
-    for x in SUBJECTNUMBER_LIST: # Проверка на номер лабораторной
-       # texttofind=r"\S+ +\d"
-       # result=re.findall(texttofind,head_of_msg)
-       # if result!=[]:
-        a = head_of_msg.lower().find(x)
-        if a != -1:
-            a = head_of_msg.find('№')
-            Number=head_of_msg[a+1]
-            break
-    if result==[]:
-        log_method.logger.warning('validation: The lab number \
-                                   is incorrect.')
-        Errors_list.append('неверно указан номер ЛР')
-        Number = None
-    
-    for x in GREATING_LIST: # Проверка на приветствие
-        texttofind=r"\S+\d+\s"
-        result=re.findall(texttofind,head_of_msg)
-       # if result!=[]: 
-       # a = body_of_msg.lower().find(x)
-        if a != -1:
-            break
-    if result!=[]:
-        log_method.logger.warning('validation: Missing greeting.')
-        Errors_list.append('отсутствует приветствие')
-    URL=url_cheack(Number,body_of_msg) # Проверка на URL
-    a = body_of_msg.find('--') # Проверка на подпись
-    #if result!=[]:
-    #    log_method.logger.warning('validation: Missing signature.')
-    #    Errors_list.append('отсутствует подпись')
-    validation_dictionary={ # Словарь
-     'Number':Number,
-     'URL': URL,
-     "errorDescription": Errors_list
-    }
-    return(validation_dictionary)
-    
-@log_method.log_method_info
-def url_cheack(Number,body_of_msg): # Проверка на наличие URL. Возврат ссылки.
-    for x in SUBJECTNUMBERURL_LIST:  # Проверка на содержание URL
-        if Number == x:
-            a = body_of_msg.find('http')
-            counter = 0
-            while  a + counter < len(body_of_msg)-1:# Ищем пробел после http
-                if body_of_msg[a + counter] != ' ':
-                    counter = counter + 1
-                else:
-                    break        
-            if a+counter == len(body_of_msg)-1: # Дошли не найдя пробелов
-                if body_of_msg[a + counter] == '.':
-                    URL = body_of_msg[a:a + counter]
-                    return(URL)
-                else:
-                    URL=body_of_msg[a:a + counter + 1]
-                    return(URL) 
-            while a + counter >= a:
-                if body_of_msg[a + counter] == '.':
-                    URL = body_of_msg[a:a + counter]
-                    logging.debug('url = %s' % URL)
-                    return (URL)                  
-                if body_of_msg[a + counter] != '.':
-                    counter = counter - 1  
+def validation(head_of_msg,body_of_msg,name):
+    """
+    Проверка полученного письма на все возможные ошибки
+    :param head_of_msg: тема письма (заголовок)
+    :param body_of_msg: тело письма, основной текст
+    name= Фамилия и имя студента
+    :return: { 'Number', 'URL', "errorDescription" }
+    """
+    url = []
+    number = ''
+    Error = []
+    count = 0
+    if (re.search(name, body_of_msg) == None): # Подпись
+        Error.append('Отсутствует подпись')
+    for i in GREATING_LIST: # Приветсвие
+        if re.search(i, body_of_msg) != None:
+            count += 1
+    if count == 0:
+        Error.append('Нет приветствия')
+    if re.search(r'№\w+', head_of_msg) != None:    # лаба
+        number = re.search(r'№\w+', head_of_msg)[0]
+    else:
+        Error.append('Неверно указан номер лабораторной')
+    if re.search(r'(ТРПО)', head_of_msg) == None:    # дисциплина
+        Error.append('Неверно указана дисциплина')
+    url.append(re.findall("(?P<url>https?://[^\s]+)",body_of_msg)) # ссылка
+    if(len(url[0]) == 0):
+        Error.append('Нет ссылки на выполненое задание')
+    validation = {
+        'Number': number,
+        'URL': url[0],
+        'Error': Error
+        }
+    return validation
