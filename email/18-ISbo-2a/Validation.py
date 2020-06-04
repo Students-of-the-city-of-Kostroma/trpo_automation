b2d465e4c864fdc9316fe69b1f7271fce9c4370f
Никита Обидин <majishpro@gmail.com>
2020-06-04 12:29:46 +0300

    добавил поиск однакоренных слов

--- email/18-ISbo-2a/Validation.py
+++ email/18-ISbo-2a/Validation.py
@@ -23,7 +23,7 @@ def validation(head_of_msg,body_of_msg,name):
             count += 1
     if count == 0:
         Error.append('Нет приветствия')
-    if re.search(r'№\w+', head_of_msg) != None:    # лаба
+    if re.search(r'№\d*', head_of_msg) != None and re.search(r'Лаб\S+', head_of_msg) != None:    # лаба
         hek=re.search(r'№\w+', head_of_msg)[0]
         hek=hek.replace('№','')
         if(int(hek)<=13 and int(hek)>0):
