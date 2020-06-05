8b38836244f61c84356dee398bf122431fd8dfa9
Алексей Жуков <gkvalex7@gmail.com>
2020-06-05 17:06:43 +0300

    try

--- LR/07/tests/getTestSuites/downloadTestSuites.sh
+++ LR/07/tests/getTestSuites/downloadTestSuites.sh
@@ -6,5 +6,5 @@ sudo pip install --upgrade pip
 # Run dowload script and create testSuites.xml file with test cases
 cd LR/07/tests/getTestSuites
 sudo python3 -m venv env
-sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+sudo pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
 sudo python3 googleDrive.py
