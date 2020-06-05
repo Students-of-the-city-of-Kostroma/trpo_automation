541fd7fa3dc6f729102b3ac4e09c9dcaa690c5b1
Алексей Жуков <gkvalex7@gmail.com>
2020-06-05 16:12:31 +0300

    try

--- LR/07/tests/getTestSuites/downloadTestSuites.sh
+++ LR/07/tests/getTestSuites/downloadTestSuites.sh
@@ -2,4 +2,6 @@
 sudo python -m venv env
 sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 sudo pip install openpyxl
+ls -la
+pwd
 sudo python googleDrive.py
