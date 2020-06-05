baa599ea714df8e14ac287c341c8d742ad49dd17
Алексей Жуков <gkvalex7@gmail.com>
2020-06-05 16:21:39 +0300

    try

--- LR/07/tests/getTestSuites/downloadTestSuites.sh
+++ LR/07/tests/getTestSuites/downloadTestSuites.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+# Upgrade Python to 3.7
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.7
+sudo apt install python3-pip
+sudo update-alternatives — install /usr/bin/python3 python3 /usr/bin/python3.7 2
+
+# Run dowload script and create testSuites.xml file with test cases
 cd LR/07/tests/getTestSuites
 sudo python -m venv env
 sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 sudo pip install openpyxl
-ls -la
-pwd
 sudo python googleDrive.py
