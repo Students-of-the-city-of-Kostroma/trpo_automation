fabb4f82af80ef039d37fb864b15258bae405173
Алексей Жуков <gkvalex7@gmail.com>
2020-06-05 17:04:12 +0300

    try

--- LR/07/tests/getTestSuites/googleDrive.py
+++ LR/07/tests/getTestSuites/googleDrive.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import pickle
 import os.path
-import openpyxl
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
