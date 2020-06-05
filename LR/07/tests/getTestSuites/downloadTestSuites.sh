8a6d36793f2960236da9743b3b78f18c16313d90
Алексей Жуков <gkvalex7@gmail.com>
2020-06-05 16:42:03 +0300

    TRY

--- LR/07/tests/getTestSuites/downloadTestSuites.sh
+++ LR/07/tests/getTestSuites/downloadTestSuites.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Run dowload script and create testSuites.xml file with test cases
+sudo apt-get install python3-venv
 cd LR/07/tests/getTestSuites
 sudo python3 -m venv env
-sudo pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-sudo pip3 install openpyxl
+sudo pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+sudo pip install openpyxl
 sudo python3 googleDrive.py
