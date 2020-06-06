dc1679e31202d06902950e0573fab2350f40dca9
Алексей Жуков <gkvalex7@gmail.com>
2020-06-06 07:38:25 +0300

    clean up

--- LR/07/tests/getTestSuites/downloadTestSuites.sh
+++ LR/07/tests/getTestSuites/downloadTestSuites.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+
 # Run dowload script and create testSuites.xml file with test cases
-#pip install virtualenv
 cd LR/07/tests/getTestSuites
-#virtualenv env
 python -m pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib --user
-pip list
 python googleDrive.py
